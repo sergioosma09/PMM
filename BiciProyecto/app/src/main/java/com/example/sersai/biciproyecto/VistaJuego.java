e406a0215637fb14de1b35d41f82d37b3942132a
sergioosma09 <sergioosma09@gmail.com>
2018-12-03 13:34:13 +0100

    Añadido EjemploFragmento y DibujoLibre

--- BiciProyecto/app/src/main/java/com/example/sersai/biciproyecto/VistaJuego.java
+++ BiciProyecto/app/src/main/java/com/example/sersai/biciproyecto/VistaJuego.java
@@ -5,31 +5,51 @@ import java.util.Vector;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
+import android.media.MediaPlayer;
 import android.util.AttributeSet;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
 
 
-public class VistaJuego extends View {
+public class VistaJuego extends View{
     //	COCHES	//
     private Vector<Grafico> Coches;	//Vector con los Coches
-    private int numCoches = 5;		//Numero inicial de Coches
-    private int numMotos = 3;		//Fragmentos/Motos en que se dividir un Coche
+    private int numCoches = 5;	//Número inicial de Coches
+    private int numMotos = 3;	//Fragmentos/Motos en que se dividirá un Coche
 
     // BICI //
     private Grafico bici;
-    private int giroBici;	//Incremento direccion de la bici
-    private float aceleracionBici;//Aumento de velocidad en la bici
+    private int giroBici;	//Incremento en la dirección de la bici
+    private float aceleracionBici;	//Aumento de velocidad en la bici
     private static final int PASO_GIRO_BICI = 5;
-    private static final float PASO_ACELERACION_BICI = 0.5f;
-    private HiloJuego hiloJuego;
+    private static final float PASO_ACELERACION_BICI = 1.5f;
 
     // THREAD Y TIEMPO //
     //Hilo encargado de procesar el tiempo
-    //private HiloJuego hiloJuego;
+    private HiloJuego hiloJuego;
     //Tiempo que debe transcurrir para procesar cambios (ms)
     private static int PERIODO_PROCESO = 50;
-    //Momento en el que se realiza el ultimo proceso
+    //Momento en el que se realizó el último proceso
     private long ultimoProceso = 0;
+
+    // PANTALLA TÁCTIL //
+    // Las variables mX y mY se utilizarán para recordar
+    // las coordenadas del último evento.
+    private float mX=0, mY=0;
+    private boolean disparo=false;
+
+    // RUEDA //
+    private Grafico rueda;
+    private static int VELOCIDAD_RUEDA = 12;
+    private boolean ruedaActiva;
+    private int distanciaRueda;
+
+    // VARIABLES GLOBALES //
+    //Controlar si la aplicación está en segundo plano
+    private boolean corriendo = false;
+    //Controlar si la aplicación está en pausa
+    private boolean pausa;
 
     public VistaJuego(Context contexto, AttributeSet atributos) {
         super(contexto, atributos);
@@ -37,9 +57,8 @@ public class VistaJuego extends View {
         //Obtenemos la imagen/recurso del coche
         graficoCoche = contexto.getResources().getDrawable(R.drawable.coche);
 
-        //Creamos un vector para contener todos los coches que se ven en la pantalla
-        //y lo rellenamos con graficos de coches
-        // con valores aleatorios para su velocidad, direccion y rotacion.
+        //vector para contener todos los coches que irán por la pantalla
+        // los coches tienen valores aleatorios en velocidad, dirección y rotación.
         Coches = new Vector<Grafico>();
         for (int i=0; i<numCoches; i++) {
             Grafico coche = new Grafico(this, graficoCoche);
@@ -53,10 +72,17 @@ public class VistaJuego extends View {
         //BICI
         graficoBici = contexto.getResources().getDrawable(R.drawable.bici);
         bici = new Grafico(this, graficoBici);
-        hiloJuego = new HiloJuego();
-        hiloJuego.start();
-    }
+        // CONTROL DEL HILO DEL JUEGO
+        corriendo = true;
 
+        // RUEDA
+
+        graficoRueda = contexto.getResources().getDrawable(R.drawable.rueda);
+
+        rueda = new Grafico(this, graficoRueda);
+        ruedaActiva = false;
+
+    }
     //Al comenzar y dibujar por primera vez la pantalla del juego
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
@@ -70,6 +96,13 @@ public class VistaJuego extends View {
             } while (coche.distancia(bici) < (w+h)/5);
         }
 
+        //Para la bici ponemos la posición central de la pantalla
+        bici.setPosX((w-bici.getAncho())/2);
+        bici.setPosY((h-bici.getAlto())/2);
+
+        //HILO QUE CONTROLA EL JUEGO
+        hiloJuego = new HiloJuego();
+        hiloJuego.start();
     }
 
     @Override
@@ -79,9 +112,13 @@ public class VistaJuego extends View {
         for (Grafico coche: Coches) {
             coche.dibujaGrafico(canvas);
         }
+        //Dibujamos la bici
         bici.dibujaGrafico(canvas);
-
+        //Dibujamos la rueda si lo indica la variable ruedaActiva
+        if (ruedaActiva)
+            rueda.dibujaGrafico(canvas);
     }
+
     protected synchronized void actualizaMovimiento() {
         long ahora = System.currentTimeMillis();
         // No hacemos nada si el período de proceso no se ha cumplido.
@@ -101,20 +138,164 @@ public class VistaJuego extends View {
             bici.setIncY(nIncY);
         }
         bici.incrementaPos();
-
+        bici.setIncX(0);
+        bici.setIncY(0);
         //Movemos los coches
         for (Grafico coche : Coches) {
             coche.incrementaPos();
         }
         ultimoProceso = ahora;
-    }
+
+        if (ruedaActiva) { //Movemos la rueda
+            rueda.incrementaPos();
+            distanciaRueda--;
+            if (distanciaRueda<0) {
+                ruedaActiva = false;
+            } else {
+                for (int i=0; i<Coches.size(); i++) {
+                    if (rueda.verificaColision(Coches.elementAt(i))) {
+                        destruyeCoche(i);
+                        i=Coches.size();
+                        ruedaActiva=false;
+                    }
+                }
+            }
+        }
+    } // del metodo actualizaMovimiento
+
     private class HiloJuego extends Thread {
         @Override
         public void run() {
             while (true) {
-                actualizaMovimiento();
+                while (corriendo) {
+                    actualizaMovimiento();
+                }
             }
         }
     }
+    // TODA LA GESTION DE Teclado y Pantalla Taltil
+    @Override
+    public boolean onKeyDown(int codigoTecla, KeyEvent evento) {
+        super.onKeyDown(codigoTecla, evento);
+        //Procesamos la pulsación
+        boolean pulsacion=true;
+        switch (codigoTecla) {
+            case KeyEvent.KEYCODE_DPAD_UP:
+                aceleracionBici=+PASO_ACELERACION_BICI;
+                break;
+            case KeyEvent.KEYCODE_DPAD_LEFT:
+                giroBici=-PASO_GIRO_BICI;
+                break;
+            case KeyEvent.KEYCODE_DPAD_RIGHT:
+                giroBici=+PASO_GIRO_BICI;
+                break;
+            case KeyEvent.KEYCODE_DPAD_CENTER:
+            case KeyEvent.KEYCODE_ENTER:
+                lanzarRueda();
+                break;
+            default:
+                //Si estamos aquí no hemos pulsado nada que nos interese
+                pulsacion=false;
+                break;
+        }
+        return pulsacion;
+    }
 
+    @Override
+    public boolean onKeyUp(int codigoTecla, KeyEvent evento) {
+        super.onKeyUp(codigoTecla, evento);
+        //Procesamos la pulsación
+        boolean pulsacion=true;
+        switch (codigoTecla) {
+            case KeyEvent.KEYCODE_DPAD_UP:
+                //aceleracionBici=0;
+                break;
+            case KeyEvent.KEYCODE_DPAD_LEFT:
+            case KeyEvent.KEYCODE_DPAD_RIGHT:
+                giroBici=0;
+                break;
+            default:
+                //Si estamos aquí, no hemos pulsado nada que interese
+                pulsacion=false;
+                break;
+        }
+        return pulsacion;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent evento) {
+        super.onTouchEvent(evento);
+        //Obtenemos la posición de la pulsación
+        float x=evento.getX();
+        float y=evento.getY();
+        switch (evento.getAction()) {
+            //Al comenzar pulsación (ACTION_DOWN) se activa la variable disparo
+            case MotionEvent.ACTION_DOWN:
+                disparo=true;
+                break;
+            //Comprobar pulsación continuada con desplazamiento hor/ver.
+            //Si es asi, desactivamos disparo: se tratará de un movimiento
+            //se trata de un movimiento en vez de  un disparo.
+            case MotionEvent.ACTION_MOVE:
+                float dx=Math.abs(x-mX);
+                float dy=Math.abs(y-mY);
+                //Un desplazamiento del dedo horizontal hace girar la bici.
+                if (dy<6 && dx>6)
+                {
+                    giroBici = Math.round((x-mX)/2);
+                    disparo = false;
+                } else //Un desplazamiento vertical produce una aceleración.
+                    if (dx<6 && dy>6)
+                    {
+                        aceleracionBici = Math.round((mY-y)/25);
+                        disparo = false;
+                    }
+                break;
+            //Si se levanta el dedo (ACTION_UP) sin haberse producido desplazamiento horizontal o vertical
+            //disparo estará activado y lo que hacemos es disparar
+            case MotionEvent.ACTION_UP:
+                giroBici = 0;
+                aceleracionBici = 0;
+                if (disparo){
+                    lanzarRueda();
+                }
+                break;
+        }
+        mX=x; mY=y;
+        return true;
+    }
+
+
+    private void destruyeCoche(int i) {
+        Coches.remove(i);
+        ruedaActiva = false;
+        //Activamos el sonido de explosión
+        MediaPlayer miMediaPlayer =
+                MediaPlayer.create(getContext(), R.raw.explosion);
+        miMediaPlayer.start();
+    }
+
+    private void lanzarRueda() {
+        rueda.setPosX(bici.getPosX() + bici.getAncho()/2 - rueda.getAncho()/2);
+        rueda.setPosY(bici.getPosY() + bici.getAlto()/2 - rueda.getAlto()/2);
+        rueda.setAngulo(bici.getAngulo());
+        rueda.setIncX(Math.cos(Math.toRadians(rueda.getAngulo())) * VELOCIDAD_RUEDA);
+        rueda.setIncY(Math.sin(Math.toRadians(rueda.getAngulo())) * VELOCIDAD_RUEDA);
+        distanciaRueda = (int)Math.min(
+                this.getWidth() / Math.abs(rueda.getIncX()),
+                this.getHeight() / Math.abs(rueda.getIncY())) - 2;
+        ruedaActiva = true;
+    }
+
+    public HiloJuego getHilo(){
+        return hiloJuego;
+    }
+
+    public void setCorriendo(boolean corriendo) {
+        this.corriendo = corriendo;
+    }
+
+    public void setPausa(boolean pausa) {
+        this.pausa = pausa;
+    }
 }
---
+++ FragmentoBasic/app/src/main/java/com/example/sersai/fragmentobasic/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.sersai.fragmentobasic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+
+public class MainActivity extends AppCompatActivity {
+    View miFragmento;
+    CheckBox apareceFrag;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        miFragmento=(View) findViewById(R.id.ejemplo);
+        apareceFrag=(CheckBox) findViewById(R.id.checkFrag);
+
+        apareceFrag.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean b) {
+                if(apareceFrag.isChecked()) miFragmento.setVisibility(View.VISIBLE);
+                else miFragmento.setVisibility(View.INVISIBLE);
+            }
+        });
+    }
+}
